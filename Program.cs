ffe819b5a731e6eb0810dd3e45648aef6b06e04a
iksiks <iksiks30@gmail.com>
2020-11-07 20:09:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Советы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sovet = new string[5];
+            sovet [0] = "1";
+            sovet [1] = "a";
+            sovet [2] = "b";
+            sovet [3] = "c";
+            sovet [4] = "d";
+
+            Random x = new Random();
+            int a = int.Parse(sovet[0]);
+            Console.WriteLine(a);
+        }
+    }
+}
