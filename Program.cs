3f70cde203a6b3ea7a56eb7fee8d59d6f536437e
iksiks <iksiks30@gmail.com>
2020-11-07 20:57:08 +0300

    добавлен элемент массива

--- Program.cs
+++ Program.cs
@@ -10,22 +10,23 @@ namespace Советы
     {
         static void Main(string[] args)
         {
-            string[] sovet = new string[6];
+            string[] sovet = new string[7];
             sovet [0] = "1";
             sovet [1] = "2";
             sovet [2] = "3";
             sovet [3] = "4";
             sovet [4] = "5";
             sovet [5] = "6";
+            sovet [6] = "7";
 
             Random x = new Random();
             for (int i = 0; i < sovet.Length; i++)
             {
                int a = int.Parse(sovet[i]);
                 Console.WriteLine(a);
-
             }
             Console.ReadKey();
+
         }
     }
 }
